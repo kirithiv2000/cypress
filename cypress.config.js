aaa7d0bfc5e649e51d77d18e5e0b0b3bfd2a08c8
kirithiv2000 <kirithiv@navgurukul.org>
2023-10-12 15:44:55 +0530

    workflow fix

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+// Populate process.env with values from .env file
+  env: {
+    googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    googleClientId: process.env.REACT_APP_GOOGLE_CLIENTID,
+    googleClientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
+  },
+});
